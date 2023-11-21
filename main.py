e30a3ea802fb767bfeefdfa002799b6fa63359af
Zacharchenko <v0666126695@gmail.com>
2023-11-21 11:26:48 +0200

    1.1.1

--- main.py
+++ main.py
@@ -1,114 +1,81 @@
-# Чи працює код - не знаю.
-# Стрьомно на робочому компі запускати )))))
-
-
 import os
 import shutil
 import re
 import sys
-
+import zipfile
+global CYRILLIC_SYMBOLS
+global TRANSLATION
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u","f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 
 def normalize(filename):
+       # Проводимо транслітерацію кирилиці на латиницю
 
-    #транслітерацію
-    filename = re.sub(r"[\u0400-\u04FF]", lambda m: chr(m.group(0).lower() + 32), filename)
+    # filename = re.sub(r"[\u0400-\u04FF]", lambda m: chr(m.group(0).lower() + 32), filename)
+    translated_name = ""
 
-    # всі символи, крім латинських літер та цифр на '_'
-    filename = re.sub(r"[^a-zA-Z0-9_]", "_", filename)
+    for c in filename:
+        if c in CYRILLIC_SYMBOLS:
+            translated_name += TRANSLATION[CYRILLIC_SYMBOLS.find(c)]
+        elif c.lower() in CYRILLIC_SYMBOLS:
+            
+            translated_name += (TRANSLATION[CYRILLIC_SYMBOLS.find(c.lower())]).capitalize()
+        else:
+            translated_name += c
+    
+    
+    filename = translated_name
 
-   
+    # Замінюємо всі символи, крім латинських літер та цифр, на '_'
+
+    filename = re.sub(r"[^a-zA-Z0-9_:.\\]", "_", filename)
+
     return filename
 
+def process_folder(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            _, extension = os.path.splitext(file)
+            normalized_name = normalize(file)
 
-def process_dir(path):
+            if extension.lower() in ['.jpg', '.jpeg', '.png', '.svg']:
+                destination = 'images'
+            elif extension.lower() in ['.avi', '.mp4', '.mov', '.mkv']:
+                destination = 'video'
+            elif extension.lower() in ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx']:
+                destination = 'documents'
+            elif extension.lower() in ['.mp3', '.ogg', '.wav', '.amr']:
+                destination = 'audio'
+            elif extension.lower() in ['.zip', '.gz', '.tar']:
+                continue
+                # destination = 'archives'
+                # extract_path = os.path.join(destination, os.path.splitext(normalized_name)[0])
+                # with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                #     zip_ref.extractall(extract_path)
+                # continue
+            else:
+                continue
+                # destination = 'unknown'
 
-    path = normalize(path)
+            destination_path = os.path.join(destination, normalized_name)
+            new_path = os.path.join(folder_path, destination_path)
 
-    if path in ["archives", "video", "audio", "documents", "images"]:
-        return
+            up_path = os.path.join(folder_path, destination)
 
-    for filename in os.listdir(path):
+            if not os.path.exists(up_path):
+                os.makedirs(up_path)
 
-        filename = normalize(filename)
+            shutil.move(file_path, new_path)
 
-        extension = os.path.splitext(filename)[1]
+    for dir in dirs:
+        dir_path = os.path.join(root, dir)
+        process_folder(dir_path)
 
-        file_type = None
-        for known_type in ["JPEG", "PNG", "JPG", "SVG", "AVI", "MP4", "MOV", "MKV", "DOC", "DOCX", "TXT", "PDF", "XLSX", "XLS", "PPTX", "MP3", "OGG", "WAV", "AMR", "ZIP", "GZ", "TAR"]:
-            if extension == known_type:
-                file_type = known_type
-                break
-
-        if file_type == "JPEG" or file_type == "PNG" or file_type == "JPG" or file_type == "SVG":
-
-            process_image(os.path.join(path, filename), file_type)
-        elif file_type == "AVI" or file_type == "MP4" or file_type == "MOV" or file_type == "MKV":
-
-            process_video(os.path.join(path, filename), file_type)
-        elif file_type == "DOC" or file_type == "DOCX" or file_type == "TXT" or file_type == "PDF" or file_type == "XLSX" or file_type == "XLS" or file_type == "PPTX":
-
-            process_document(os.path.join(path, filename), file_type)
-        elif file_type == "MP3" or file_type == "OGG" or file_type == "WAV" or file_type == "AMR":
-
-            process_music(os.path.join(path, filename), file_type)
-        elif file_type == "ZIP" or file_type == "GZ" or file_type == "TAR":
-
-            process_archive(os.path.join(path, filename), file_type)
-        else:
-  
-            process_unknown(os.path.join(path, filename), file_type)
-
-    for subpath in os.listdir(path):
-
-        if not os.listdir(os.path.join(path, subpath)):
-
-            shutil.rmdir(os.path.join(path, subpath))
-
-        process_dir(os.path.join(path, subpath))
-
-def process_image(path, file_type):
-
-    target_path = os.path.join("images", os.path.basename(path))
-    shutil.move(path, target_path)
-
-def process_video(path, file_type):
-
-    target_path = os.path.join("video", os.path.basename(path))
-    shutil.move(path, target_path)
-
-def process_document(path, file_type):
-
-    target_path = os.path.join("documents", os.path.basename(path))
-    shutil.move(path, target_path)
-
-def process_music(path, file_type):
-
-    target_path = os.path.join("audio", os.path.basename(path))
-    shutil.move(path, target_path)
-
-def process_archive(path, file_type):
-
-    target_path = os.path.join("archives", os.path.splitext(os.path.basename(path))[0])
-    shutil.unpack_archive(path, target_path)
-
-def process_unknown(path, file_type):
-
-    # Файл залишаємо на місці
-    pass
+def main():
+    # target_folder = input("Введіть шлях до папки для сортування: ")
+    target_folder ="C:\\TEST"
+    process_folder(target_folder)
 
 if __name__ == "__main__":
-
-    # target_path = sys.argv[1]
-    target_path = f"C:\ТЕСТ\"
-
-    target_path = normalize(target_path)
-
-    os.chdir(target_path)
-
-    process_dir(os.getcwd())
-
-    for path, directories, files in os.walk(os.getcwd()):
-       
-        if not files and not directories:
-            
-            shutil.rmdir(path)
+    main()

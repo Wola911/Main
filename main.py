da22c043a3f28a2c084387ec1e4deb058f6117f7
Zacharchenko <v0666126695@gmail.com>
2023-12-27 21:10:27 +0200

    1.0.1

--- main.py
+++ main.py
@@ -1,83 +1,57 @@
 from datetime import date, datetime, timedelta
 
-
 def get_birthdays_per_week(users):
     
+    week_dict = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday"}
+    names_dict = {0:[], 1:[], 2:[], 3:[], 4:[]}
+
     print("START")
     print(users)
 
-    users_new = {}
+    
+
+
+
+    # users_new = {}
     users_dict = {}
 
-    m_monday = []
-    m_tuesday = []
-    m_wednesday = []
-    m_thursday = []
-    m_friday = []
+    # m_monday = []
+    # m_tuesday = []
+    # m_wednesday = []
+    # m_thursday = []
+    # m_friday = []
 
     if len(users) == 0:
-        return users_new
+        return users_dict
 
     today = date.today()
-    this_week = today.weekday()
+    # n_week = today.weekday()
 
-    l_set = 5 - this_week
-    l_sun = 6 - this_week
-    end_next_week = l_sun + 5
+    # l_set = 5 - this_week
+    # l_sun = 6 - this_week
+    # end_next_week = l_sun + 5
 
-    for m_list in users:
+   
 
-        m_name = m_list["name"]
-        m_birthday = m_list["birthday"]
+    for i in range(0, 7):
 
-        m_date = today + timedelta(days=l_set)
-        if m_birthday.month == m_date.month and m_birthday.day == m_date.day:
-            # users_dict["Monday"].append(m_name)
-            m_monday.append(m_name)
-            continue
+        m_date = today + timedelta(days=i) 
+        m_weekday = m_date.weekday()
+        
+        for user in users:
 
-        m_date = today + timedelta(days=l_sun)
-        if m_birthday.month == m_date.month and m_birthday.day == m_date.day:
-            # users_dict["Monday"].append(m_name)
-            m_monday.append(m_name)
-            continue
+            m_name = user["name"]
+            m_birthday = user["birthday"]
+            
+            if m_birthday.month == m_date.month and m_birthday.day == m_date.day:
+                if m_weekday == 5 or m_weekday == 6:
+                    names_dict[0].append(m_name)
+                else:
+                    names_dict[m_weekday].append(m_name)
 
-        if m_birthday.month == (today + timedelta(days=(l_sun + 1))).month and m_birthday.day == (today + timedelta(days=(l_sun+1))).day:
-            # users_dict["Monday"].append(m_name)
-            m_monday.append(m_name)
-        elif m_birthday.month == (today + timedelta(days=(l_sun + 2))).month and m_birthday.day == (today + timedelta(days=(l_sun+2))).day:
-            # users_dict["Tuesday"].append(m_name)
-            m_tuesday.append(m_name)
-        elif m_birthday.month == (today + timedelta(days=(l_sun + 3))).month and m_birthday.day == (today + timedelta(days=(l_sun+3))).day:
-            # users_dict["Wednesday"].append(m_name)
-            m_wednesday.append(m_name)
-        elif m_birthday.month == (today + timedelta(days=(l_sun + 4))).month and m_birthday.day == (today + timedelta(days=(l_sun+4))).day:
-            # users_dict["Thursday"].append(m_name)
-            m_thursday.append(m_name)
-        elif m_birthday.month == (today + timedelta(days=(l_sun + 5))).month and m_birthday.day == (today + timedelta(days=(l_sun+5))).day:
-            # users_dict["Friday"].append(m_name)
-            m_friday.append(m_name)
-
-    
-    if len(m_monday) > 0:
-       m_dict = {"Monday": m_monday}
-       users_dict.update(m_dict) 
-
-    if len(m_tuesday) > 0:
-       m_dict = {"Tuesday": m_tuesday}
-       users_dict.update(m_dict) 
-
-    if len(m_wednesday) > 0:
-       m_dict = {"Wednesday": m_wednesday}
-       users_dict.update(m_dict) 
-
-    if len(m_thursday) > 0:
-       m_dict = {"Thursday": m_thursday}
-       users_dict.update(m_dict) 
-
-    if len(m_friday) > 0:
-       m_dict = {"Friday": m_friday}
-       users_dict.update(m_dict) 
+        if not(m_weekday == 5 or m_weekday == 6) and len(names_dict[m_weekday]) > 0:
+            new_dict = {week_dict[m_weekday]:names_dict[m_weekday]}
+            users_dict.update(new_dict)
 
     users = users_dict
 
@@ -88,10 +62,21 @@ def get_birthdays_per_week(users):
 
 
 if __name__ == "__main__":
+    # users = [
+    #     {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+    # ]
+    
     users = [
-        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+            {
+                "name": "John",
+                "birthday": (date.today() + timedelta(days=4)),
+            },
+            # {
+            #     "name": "Doe",
+            #     "birthday": (self.today + timedelta(days=6)).date(),
+            # },
+            # {"name": "Alice", "birthday": (self.today + timedelta(days=3)).date()},
     ]
-
     result = get_birthdays_per_week(users)
     print(result)
     # Виводимо результат
